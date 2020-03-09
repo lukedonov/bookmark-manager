a45887826f97c423f1fea1b84b50a38f109840f6
Luke Donovan <lukelamnisos@icloud.com>
2020-03-09 16:28:03 +0000

    added first tests

---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,13 @@
+feature 'viewing bookmarks' do
+  scenario "visitng the index page" do
+    visit('/')
+    expect(page).to have_content "Bookmark Manager"
+  end
+  scenario 'A user can see bookmarks' do
+    visit('/bookmarks')
+
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com"
+  end
+end
