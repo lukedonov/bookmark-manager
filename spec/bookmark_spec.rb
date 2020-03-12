ca282fc85010fa6dca443790b6d0fcc7f78c0432
Luke Donovan <lukelamnisos@icloud.com>
2020-03-12 16:04:53 +0000

    tests pass

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -42,4 +42,16 @@ describe Bookmark do
       expect(Bookmark.all.length).to eq 0
     end
   end
+
+  # describe '.update' do
+  #   it "edits existing bookmarks" do
+  #     bookmark = Bookmark.create(url: 'http://www.facebook.com', title: 'Facebook')
+  #     updated_bookmark = Bookmark.update(id: bookmark.id, url: 'wwww.twitter.com', title: 'Twitter')
+
+  #     expect(updated_bookmark).to be_a Bookmark
+  #     expect(updated_bookmark.id).to eq bookmark.id
+  #     expect(updated_bookmark.title).to eq 'Twitter'
+  #     expect(updated_bookmark.url).to eq 'http://www.Twitter.com'
+  #   end
+  # end
 end
