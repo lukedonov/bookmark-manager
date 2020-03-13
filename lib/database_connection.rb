f25fabfd4997b28efc544f96a319c76ebf52294a
Luke Donovan <lukelamnisos@icloud.com>
2020-03-13 14:54:44 +0000

    refactoring

---
+++ lib/database_connection.rb
@@ -0,0 +1,15 @@
+require 'pg'
+
+class DatabaseConnection
+  def self.setup(dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
+
+  def self.connection
+    @connection
+  end
+
+  def self.query(query)
+    @connection.exec(query)
+  end
+end
